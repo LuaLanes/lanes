56732a75af96315f4f3c9045459d5b19433a9054
sonoro1234 <sonoro@telefonica.net>
2018-10-30 19:57:23 +0100

    set protect_allocator default for several LuaJIT

--- src/lanes.lua
+++ src/lanes.lua
@@ -66,7 +66,8 @@ lanes.configure = function( settings_)
 	local pairs = assert( pairs)
 	local tostring = assert( tostring)
 	local error = assert( error)
-
+	local ffi
+	if package.loaded.jit and jit.version then ffi=require"ffi" end
 	local default_params =
 	{
 		nb_keepers = 1,
@@ -77,7 +78,7 @@ lanes.configure = function( settings_)
 		demote_full_userdata = nil,
 		verbose_errors = false,
 		-- LuaJIT provides a thread-unsafe allocator by default, so we need to protect it when used in parallel lanes
-		protect_allocator = (package.loaded.jit and jit.version) and true or false
+		protect_allocator = (package.loaded.jit and jit.version and (ffi.abi"32bit" or (ffi.abi"64bit" and ffi.abi"gc64"))) and true or false
 	}
 	local boolean_param_checker = function( val_)
 		-- non-'boolean-false' should be 'boolean-true' or nil
