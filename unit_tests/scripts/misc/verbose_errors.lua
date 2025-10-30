9c768185696879c6642c1f26c86884eaf727fe27
Benoit Germain <benoit.germain@ubisoft.com>
2025-10-30 09:38:43 +0100

    Stronger test for verbose_errors

--- unit_tests/scripts/misc/verbose_errors.lua
+++ unit_tests/scripts/misc/verbose_errors.lua
@@ -4,23 +4,27 @@ local l = lanes.linda{name = "my linda"}
 
 
 local do_test = function(key_)
-	local t = {[key_] = fixture.newuserdata()}
+	local t = { subtable = { [key_] = { ud = fixture.newuserdata() } } }
 	local b, e = pcall(l.send, l, "k", t)
 	assert(b == false and type(e) == "string")
 	local t_key = type(key_)
 	if t_key == "string" then
-		local x, y = string.find(e, "arg#2." .. key_, 1, true)
+		-- expecting an error about arg#2.subtable.<key_>.ud
+		local x, y = string.find(e, "arg#2.subtable." .. key_ .. ".ud", 1, true)
 		assert(x and y, "got " .. e)
 	elseif t_key == "boolean" then
-		local x, y = string.find(e, "arg#2[" .. tostring(key_) .. "]", 1, true)
+		-- expecting an error about arg#2.subtable[true|false].ud
+		local x, y = string.find(e, "arg#2.subtable[" .. tostring(key_) .. "].ud", 1, true)
 		assert(x and y, "got " .. e)
 	elseif t_key == "number" then
-		local x, y = string.find(e, "arg#2[" .. key_ .. "]", 1, true)
+		-- expecting an error about arg#2.subtable[<number>].ud
+		local x, y = string.find(e, "arg#2.subtable[" .. key_ .. "].ud", 1, true)
 		assert(x and y, "got " .. e)
 	elseif t_key == "userdata" then
 		local t_name
 		-- light userdata is formatted by std::format, where the pointer is written as a lowercase hex literal
-		local expected = "arg#2[U:0x" .. string.lower(string.match(tostring(key_), "userdata: (%x+)")) .. "]"
+		local expected = "arg#2.subtable[U:0x" .. string.lower(string.match(tostring(key_), "userdata: (%x+)")) .. "].ud"
+		-- expecting an error about arg#2.subtable[U:0x<some hex value>].ud
 		local x, y = string.find(e, expected, 1, true)
 		assert(x and y, "expecting " .. expected .. " got " .. e)
 	end
